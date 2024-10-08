3b26b38acae838338fc85ab0befc4135057b138b
Assaf Itzikson <assaf.itzikson@immediate.co.uk>
2024-10-08 13:58:12 +0100

    refactoring a bit more

--- index.js
+++ index.js
@@ -2,12 +2,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const quizRouter = require('./routes/quiz');
 
 const app = express();
 app.use(bodyParser.json());
-
-app.use('/api', quizRouter);
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
